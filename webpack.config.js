4f8f4c90776be3834da5c24a5d53b6422710da99
Nick <nlchan168@yahoo.com>
2023-02-28 03:11:31 -0500

    added loaders and testing css

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
 const path = require("path");
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
   mode: "development",
   entry: "./src/index.js",
   output: {
